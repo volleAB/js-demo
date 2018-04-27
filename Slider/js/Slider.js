8c8eec84d7d0ec4fbfe98dabc65b952a42af683a
volleAB <724958674@qq.com>
2018-04-27 22:10:37 +0800

    style complete

--- Slider/js/Slider.js
+++ Slider/js/Slider.js
@@ -1,2 +1,14 @@
+// Slider
 
-
+var oCar = document.getElementById('carousel');
+var oPre = document.getElementById('pre');
+var oNext = document.getElementById('next');
+var oItems = document.getElementById('caro-items');
+var sDots = document.getElementById('control');
+var aDots = document.getElementById('control').getElementsByTagName('li');
+var aImg = oItems.getElementsByTagName('img');
+var index = null;
+//图片切换函数
+function (pre,mext){
+	oItems.style.width= aImg[0].offsetWidth * aImg.length + 'px';
+}
