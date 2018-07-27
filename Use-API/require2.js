3c6d289af0283615115edba903068fa660ac0150
volleAB <724958674@qq.com>
2018-07-27 23:08:27 +0800

    lol

--- Use-API/require2.js
+++ Use-API/require2.js
@@ -1,6 +1,6 @@
-var pageList = [];
-var oi = $('.img');
-var text = $('.div1');
+// var pageList = [];
+// var oi = $('.img');
+// var text = $('.div1');
 var text2 = document.getElementsByTagName('div')[0];
 
 const promise = (method, url, data) => {
@@ -22,15 +22,15 @@ const promise = (method, url, data) => {
 
 const jsonData = '';
 
-const firstAjax = promise('get', 'https://yesno.wtf/api', jsonData)
+const firstAjax = promise('get', 'http://result.eolinker.com/RUkKcGwf7e0080fef9236d792f9d5d2a0f8b8961a9837b1?uri=/home', jsonData)
 firstAjax.then((res) => {
-	let link = res.image;
-	let img = new Image();
-	img.onload=function(){alert("img is loaded")};  
-    img.onerror=function(){alert("error!")};
-    img.src=link;
-    oi.src = link;
-	text.html(res.answer);
+	// let link = res.image;
+	// let img = new Image();
+	// img.onload=function(){alert("img is loaded")};  
+    // img.onerror=function(){alert("error!")};
+    // img.src=link;
+    // oi.src = link;
+	// text.html(res.ans、wer);
 	// oi.src = link;	//todo
-	console.log(link);
+	console.log(res);
 });
