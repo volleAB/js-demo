a4ee46cd085751d4f7a5445a9a2a9b1953542d86
volleAB <724958674@qq.com>
2018-10-18 21:11:02 +0800

    new

--- Slider2/script.js
+++ Slider2/script.js
@@ -1,39 +1,41 @@
-let itemCon = $('.s-container')
-let aItem = $('.s-container .item')
-let timer = null
-let limit = 2*parseInt(aItem.css('margin')) + parseInt(aItem.css('width'))
-// console.log(limit)
 
-move = (speed, time, num) => {
+function produce (itemCon, aItem) {
+    let thisItemCon = $(itemCon)
+    let thisItem = $(aItem)
+    let timer = null
+    let limit = 2*parseInt(thisItem.css('margin')) + parseInt(thisItem.css('width'))
+
+    move = (speed, time, num) => {
     let i = 0
-    let nowX = itemCon.css('transform').replace(/[^0-9\-,]/g,'').split(',')[4]
+    let nowX = thisItemCon.css('transform').replace(/[^0-9\-,]/g,'').split(',')[4]
     nowX = parseInt(nowX)
     itemMove = () => {
         setTimeout(() => {
-            itemCon.css('transform', 'translateX(' + (nowX - speed) + 'px)')
+            thisItemCon.css('transform', 'translateX(' + (nowX - speed) + 'px)')
         }, time)
         if(nowX <= -(limit*num)) {
-            itemCon.css('transform', 'translateX(0px)')
+            thisItemCon.css('transform', 'translateX(0px)')
             nowX = 0
         }
     }
-    itemMove()
+        itemMove()
+    }
+
+    goMove = () => {
+        move(1, 10, 8)
+    }
+
+    thisItem.mouseover(() => {
+        clearInterval(timer)
+    })
+
+    thisItem.mouseout(() => {
+        timer = setInterval(goMove, 10)
+    })
+
+    startMove = () => {
+        timer = setInterval(goMove, 10)
+    }
+
+    startMove()
 }
-
-goMove = () => {
-    move(1, 10, 8)
-}
-
-aItem.mouseover(() => {
-    clearInterval(timer)
-})
-
-aItem.mouseout(() => {
-    timer = setInterval(goMove, 10)
-})
-
-startMove = () => {
-    timer = setInterval(goMove, 10)
-}
-
-startMove()
